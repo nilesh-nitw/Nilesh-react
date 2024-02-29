abf2c07c7c3a5fd5cc34fd29303f4e65c0462388
Nilesh Maurya <nileshmaurya789@gmail.com>
2024-02-29 14:27:31 +0530

    third lecture code

--- App.js
+++ App.js
@@ -1,17 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id :"parent"},
-[React.createElement("div",{id :"child"}, 
-[React.createElement("h1",{},"i am h1 tag"),
-React.createElement("h2",{},"i am h2 tag")]),
-React.createElement("div",{id :"child2"}, 
-[React.createElement("h1",{},"i am h1 tag"),
-React.createElement("h2",{},"i am h2 tag")])]);
+// react Element
+
+const elem =<span> React Eelemt </span>
+
+const title= (
+    <h1 className ="head" tableIndex ="5">  
+    {elem}
+    React by Nilesh title example
+    </h1>
+    );
 
 
-const heading= React.createElement("h1",
-{id :"heading"},
-"Hello world from reacts");
- const root=ReactDOM.createRoot(document.getElementById("root"));
- root.render(parent);
+// Tital component
+const Title =() =>(
+<h1 className ="head" tableIndex ="5">  
+React by Nilesh component injwction
+</h1>
+);
+
+//React functional Component
+
+// class based components old will learn later not required for new devlopment
+//functional component -new
+
+
+//component composition
+
+const number =1000;
+
+const HeadingComponent= ()=>(
+    <div id ="container">
+        <h2>{number}</h2>
+        {title}
+         {/* this way can put another componet   */}
+        <Title />
+        <Title></Title>
+        {Title()}
+     <h1 className="heading"> Nilesh react functional component</h1>
+     </div>
+);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading);
+root.render(<HeadingComponent/>)
