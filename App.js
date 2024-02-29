34861f8dbcb33d2ce813af8ad1c6ad3c867ce115
Nilesh Maurya <nileshmaurya789@gmail.com>
2024-02-29 10:52:09 +0530

    second lecture

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div",{id :"parent"},
 [React.createElement("div",{id :"child"}, 
@@ -7,8 +9,9 @@ React.createElement("div",{id :"child2"},
 [React.createElement("h1",{},"i am h1 tag"),
 React.createElement("h2",{},"i am h2 tag")])]);
 
+
 const heading= React.createElement("h1",
 {id :"heading"},
-"Hello world from react");
+"Hello world from reacts");
  const root=ReactDOM.createRoot(document.getElementById("root"));
  root.render(parent);
